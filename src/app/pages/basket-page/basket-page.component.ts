64875b755a2aacf5a99f46b10ac0ad7748c20e59
antonio_tomas\Usuario <antoniotomassalinas@gmail.com>
2023-07-14 13:25:43 +0200

    Paginas y componentes creados

---
+++ src/app/pages/basket-page/basket-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-basket-page',
+  templateUrl: './basket-page.component.html',
+  styleUrls: ['./basket-page.component.scss']
+})
+export class BasketPageComponent {
+
+}
