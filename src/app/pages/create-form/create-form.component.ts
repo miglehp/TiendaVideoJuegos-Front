fd0bf73c28d7423bd67f956247de8acd187b6b5f
Enrique <25wike25@gmail.com>
2023-07-17 09:40:37 +0200

    bootstrap instalado

--- src/app/pages/create-form/create-form.component.ts
+++ src/app/pages/create-form/create-form.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-
+import { FormGroup } from '@angular/forms'
 @Component({
   selector: 'app-create-form',
   templateUrl: './create-form.component.html',
   styleUrls: ['./create-form.component.scss']
 })
 export class CreateFormComponent {
+
+  formulario: FormGroup;
 
 }
