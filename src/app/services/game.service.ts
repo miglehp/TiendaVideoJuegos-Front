9bd6183e6e0ad45ea219b755d1a6121d5c7ddb81
miglehp <migle.hp@gmail.com>
2023-07-19 11:50:50 +0200

    corregida función de getGameById en game.service y formateo del fichero

--- src/app/services/game.service.ts
+++ src/app/services/game.service.ts
@@ -1,34 +1,30 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { firstValueFrom } from 'rxjs';
-import { GAMES} from '../db/games.db';
+import { GAMES } from '../db/games.db';
 import { Game } from '../interfaces/game.interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class GameService {
-
   httpClient = inject(HttpClient);
   baseUrl: string;
 
-  constructor() { 
-    this.baseUrl = 'http://localhost:3000/api/games'
+  constructor() {
+    this.baseUrl = 'http://localhost:3000/api/games';
   }
   getAll(): Promise<Game[]> {
-    return firstValueFrom(
-      this.httpClient.get<Game[]>(this.baseUrl)
-    )
+    return firstValueFrom(this.httpClient.get<Game[]>(this.baseUrl));
   }
 
   registro(formValue: any): Promise<Game> {
     return firstValueFrom(
       this.httpClient.post<Game>(`${this.baseUrl}`, formValue)
-    )
+    );
   }
 
-  getById(id: number): Game | undefined {
-      return GAMES.find(game => game.id === id);
-    }
-
+  getById(id: number): Promise<Game> {
+    return firstValueFrom(this.httpClient.get<Game>(this.baseUrl + '/' + id));
+  }
 }
