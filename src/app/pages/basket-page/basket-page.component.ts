34432b78f41421e4e2ef163a45e7dd11ff79d40d
Enrique <25wike25@gmail.com>
2023-07-20 11:47:47 +0200

    localStorage y carrito de compra creados

--- src/app/pages/basket-page/basket-page.component.ts
+++ src/app/pages/basket-page/basket-page.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Game } from 'src/app/interfaces/game.interface';
+import { BasketService } from 'src/app/services/basket.service';
 
 @Component({
   selector: 'app-basket-page',
@@ -6,5 +8,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./basket-page.component.scss']
 })
 export class BasketPageComponent {
+
+  
+
+basketService = inject(BasketService);
+
+games: Game[];
+
+  
+
+constructor(){
+  this.games = [];
+}
+
+ngOnInit() {
+  this.games = this.basketService.getAll();
+}
+
+onClickEliminar(indice: number) {
+  this.basketService.deleteGame(indice)
+}
 
 }
--- src/app/pages/order-detail/order-detail.component.ts
+++ src/app/pages/order-detail/order-detail.component.ts
@@ -1,10 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-order-detail',
   templateUrl: './order-detail.component.html',
-  styleUrls: ['./order-detail.component.scss']
+  styleUrls: ['./order-detail.component.scss'],
 })
-export class OrderDetailComponent {
-
-}
+export class OrderDetailComponent {}
