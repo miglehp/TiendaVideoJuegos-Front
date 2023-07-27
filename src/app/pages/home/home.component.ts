960e8cafe4b6946be119390efc5b56f67fce8de2
miglehp <migle.hp@gmail.com>
2023-07-27 14:07:26 +0200

    Hecho el home y algunos arreglos a estilos en user profile y en general a app.component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,8 @@
 import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { Game } from 'src/app/interfaces/game.interface';
 import { GameService } from 'src/app/services/game.service';
+import { ScreenshotService } from 'src/app/services/screenshot.service';
 
 @Component({
   selector: 'app-home',
@@ -7,5 +10,31 @@ import { GameService } from 'src/app/services/game.service';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
+  router = inject(Router)
   gameService = inject(GameService);
+  screenShotService = inject(ScreenshotService);
+
+  games: Game[];
+
+  constructor(){
+    this.games = [];
+  }
+
+  ngOnInit(){
+    this.setGames();
+  }
+
+  async setGames(){
+    const allGames = await this.gameService.getAll();
+    const number = Math.ceil(Math.random()*(allGames.length-5));
+    for(let i = 0; i < 5; i++){
+      const screenshot = await this.screenShotService.getScreenshotsById(allGames[number+i].id);
+      allGames[number+i].header_image = screenshot[0].path_full;
+      this.games.push(allGames[number+i]);
+    }
+  }
+
+  onClickNavigate(id: number){
+    this.router.navigate(['/gameDetail/'+id]);
+  }
 }
